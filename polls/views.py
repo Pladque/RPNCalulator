832a9d1bdd2bb5c52083a96066de6e48bfc7d6fe
Pladque <63751303+Pladque@users.noreply.github.com>
2021-01-14 13:47:25 +0100

    digids < 0 on first place is possible now

--- polls/views.py
+++ polls/views.py
@@ -6,6 +6,8 @@ from Scripts import functions as f
 def home_calculator(request):
     equation = request.POST.get('equation')
     if equation:
+        if list(equation)[0] == '-' or list(equation)[0] == '+':
+            equation = '0' + equation
         equation_in_RPN = f.trim_change_to_list_and_convert_to_RPN(equation)
         if len(equation_in_RPN)!= 0:
             equation_in_RPN_as_string = f.list_equation_to_str(equation_in_RPN)
